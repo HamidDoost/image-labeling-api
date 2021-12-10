bed9e2f373594f23e655cee149cd5a4bf0a507af
Hamid <hamid@MacBook-Pro.hub>
2021-12-11 07:18:58 +1100

    added morgan middleware for logging HTTP requests

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express, { Application } from "express";
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 3000;
 const app: Application = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
